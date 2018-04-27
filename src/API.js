362d7ce0f56b3ebea7e45d391227fe60402ef3d6
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 10:20:37 -0700

    Add correct lat and long, and insert strings into url

--- src/API.js
+++ src/API.js
@@ -1,12 +1,14 @@
 class API {
   constructor() {
-    this.type;
-    this.location;
+    this.type = "doctors";
+    this.doctorName;
+    this.location = "45.5231,-122.6765,10";
   }
   makeCall() {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`
+      let url = `https://api.betterdoctor.com/2016-03-01/${this.type}?location=${this.location}&limit=10&user_key=${process.env.API_KEY}`
+      console.log(url);
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
@@ -16,6 +18,9 @@ class API {
       };
       request.open("GET", url, true);
       request.send();
+    }).then((response) => {
+      let body = JSON.parse(response);
+      console.log(body);
     });
   }
 }
