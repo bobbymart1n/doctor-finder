1cdb0222a87c2e4c463bd5bf067dee010161f42d
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 09:21:43 -0700

    Add log of test api call

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-// import $ from 'jquery';
+import { API } from './API.js';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(function() {
+  let api = new API();
+  console.log(api.makeCall());
+});
