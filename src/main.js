475f0e3b806196da70089b4f579b4dd26885989e
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 11:41:47 -0700

    Add setTimeout to make sure response has come back

--- src/main.js
+++ src/main.js
@@ -12,5 +12,16 @@ $(function() {
     let api = new API();
     api.doctorName = drFirstName;
     api.makeCall();
+    $(this).hide();
+    $("#results").show();
+    $("#loading").show();
+    setTimeout(() => {
+      $("#loading").hide();
+      $("#results").prepend(`
+        <div class="doctor-item">
+        <h3>${api.searchResults.data[0].profile.first_name}</h3>
+        </div>
+        `);
+    }, 1500);
   });
 });
