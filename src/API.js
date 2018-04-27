b42807b08c1e7ceb34cfcf18775e9ed6d55c6dd9
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 10:46:02 -0700

    Add search type and symptom type change url to reflect those

--- src/API.js
+++ src/API.js
@@ -1,14 +1,14 @@
 class API {
-  constructor() {
-    this.type = "doctors";
+  constructor(type, symptom) {
+    this.type = type;
+    this.symptom = symptom;
     this.doctorName;
     this.location = "45.5231,-122.6765,10";
   }
   makeCall() {
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/${this.type}?location=${this.location}&limit=10&user_key=${process.env.API_KEY}`
-      console.log(url);
+      let url = `https://api.betterdoctor.com/2016-03-01/${this.type}?fields=${this.symptom}&location=${this.location}&limit=10&user_key=${process.env.API_KEY}`
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
