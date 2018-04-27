aeee2a8f91f0eac625d673c60fbc519f45cc7d42
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 14:34:27 -0700

    Add conditional depending on if user enters a symptom or doctors name

--- src/API.js
+++ src/API.js
@@ -1,13 +1,27 @@
 class API {
   constructor() {
+    this.url;
     this.doctorName;
+    this.symptom;
     this.searchResults;
     this.location = "45.5231,-122.6765,10";
   }
+  conditionSearch() {
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${this.symptom}&location=${this.location}&limit=10&user_key=${process.env.exports.apiKey}`;
+  }
+  nameSearch() {
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${this.doctorName}&location=${this.location}&limit=10&user_key=${process.env.exports.apiKey}`;
+  }
   makeCall() {
+    if (this.symptom) {
+      this.conditionSearch();
+      console.log(this.url);
+    } else {
+      this.nameSearch();
+      console.log(this.url);
+    }
     return new Promise((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${this.doctorName}&location=${this.location}&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
@@ -15,7 +29,7 @@ class API {
           reject(Error(request.statusText));
         }
       };
-      request.open("GET", url, true);
+      request.open("GET", this.url, true);
       request.send();
     }).then((response) => {
       let body = JSON.parse(response);
