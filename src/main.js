0b685e5547c073269fd1cbf15163115e331f5daa
Bobby Martin <robert.martin.619@gmail.com>
2018-04-27 15:34:11 -0700

    Add conditions if doctor is accepting new patients, add first and last name sets

--- src/main.js
+++ src/main.js
@@ -9,8 +9,10 @@ $(function() {
     event.preventDefault();
     const symptom = $("#symptom").val().toLowerCase();
     const drFirstName = $("#docFirstName").val().toLowerCase();
+    const drLastName = $("#docLastName").val().toLowerCase();
     let api = new API();
-    api.doctorName = drFirstName;
+    api.doctorFirstName = drFirstName;
+    api.doctorLastName = drLastName;
     api.symptom = symptom;
     api.makeCall();
     $(this).hide();
@@ -20,13 +22,20 @@ $(function() {
       $("#loading").hide();
       if(api.searchResults.data.length >= 1) {
         api.searchResults.data.map((doctor) => {
+          let newPatients;
+          if(doctor.practices[0].accepts_new_patients) {
+            newPatients = `<p>Accepting new patients: Yes</p>`
+          } else {
+            newPatients = `<p>Accepting new patients: No</p>`
+          }
           $("#results").prepend(`
             <div class="doctor-item">
               <img src=${doctor.profile.image_url} alt='A photo of Dr. ${doctor.profile.last_name}'>
               <h4>${doctor.profile.first_name} ${doctor.profile.last_name}</h4>
               <p class="mb-0">Phone: <a href="tel:${doctor.practices[0].phones[0].number}">${doctor.practices[0].phones[0].number}</a></p>
               <p class="mb-0">${doctor.practices[0].visit_address.street}</p>
-              <p>${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state}. ${doctor.practices[0].visit_address.zip}</p>
+              <p class="mb-0">${doctor.practices[0].visit_address.city} ${doctor.practices[0].visit_address.state}. ${doctor.practices[0].visit_address.zip}</p>
+              ${newPatients}
             </div>
           `);
         });
